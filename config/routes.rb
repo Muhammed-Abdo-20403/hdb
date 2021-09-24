7c0d44b8c805435389d4a60d58acd1e758a54cb7
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2021-09-24 17:36:10 +0200

    add routes  for  hospotals, governorates, cities, patients, diseases

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "dashboards#root"
+
+  resources :hospotals
+  resources :governorates
+  resources :cities
+  resources :patients
+  resources :diseases
+
 end
