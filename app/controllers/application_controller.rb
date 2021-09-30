e01c7dd21489ff11a0ea8b8527f8380a579669bb
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2021-09-30 12:35:38 +0200

    new parameters for account_update_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         attributes = [:name, :phone]
+        attributes_account_update = [:name, :phone]
         devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+        devise_parameter_sanitizer.permit(:account_update, keys: attributes_account_update)
     end
 end
