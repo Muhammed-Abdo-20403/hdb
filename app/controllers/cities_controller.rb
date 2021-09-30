e86910dc0e87dcc38004ef4d7bcae8b9dac57b0a
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2021-09-30 11:21:36 +0200

    add city create and index files

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,28 @@
 class CitiesController < ApplicationController
+    def index
+        @cities = City.page(params[:page]).per(5)
+        @city = City.new
+    end
+
+    def create 
+        @city = City.new(city_parameters)
+        if @city.save
+            redirect_to  cities_path
+        else 
+            render :index
+        end
+    end
+
+    def destroy
+        City.destroy(params[:id])
+        redirect_to cities_path
+    end
+
+
+    private
+
+
+    def city_parameters
+        params.require(:city).permit(:name, :governorate_id)
+    end
 end
