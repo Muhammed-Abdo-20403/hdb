1ae92bb5296ca0be8e579a2b9b48e60cb25e2d66
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2021-10-04 10:32:15 +0200

    Set time zone on table record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
+    around_action :set_time_zone
 
 
     protected
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
         attributes_account_update = [:name, :phone]
         devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
         devise_parameter_sanitizer.permit(:account_update, keys: attributes_account_update)
+    end
+
+    def set_time_zone
+        Time.use_zone("Cairo") { yield }
     end
 end
