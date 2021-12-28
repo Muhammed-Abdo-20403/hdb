18a750cba222f51248fac38586aa28493808bbf4
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2021-12-28 15:00:28 +0200

    Add internationalization configurations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
     around_action :set_time_zone
+    around_action :switch_locale
 
-
+    def switch_locale(&action)
+        locale = params[:locale] || I18n.default_locale
+        I18n.with_locale(locale, &action)
+    end
     protected
 
     def configure_permitted_parameters
