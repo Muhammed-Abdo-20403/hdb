6c3ec19bff9bf23fc3bf0b17d943defe8b31059a
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-05-22 11:00:43 +0200

    add more data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,29 +19,30 @@ cities_and_governorates
 create_hospital
 
 patients = []
-1000.times do |patient|
-   p = Patient.new
-   p.name = Faker::Name.name
-   p.email = Faker::Internet.email
-   p.age = Faker::Number.between(from: 1, to: 130)
-   p.sex = Faker::Gender.binary_type
-   p.phone = Faker::PhoneNumber.cell_phone_in_e164
-   p.city_id = rand(1..183)
-   patients << p
+10000.times do |patient|
+   fake_patients = Patient.new
+   fake_patients.name = Faker::Name.name
+   fake_patients.email = Faker::Internet.email
+   fake_patients.age = Faker::Number.between(from: 1, to: 130)
+   fake_patients.gender = Faker::Gender.binary_type
+   fake_patients.phone = Faker::PhoneNumber.cell_phone_in_e164
+   fake_patients.city_id = rand(1..183)
+   patients << fake_patients
 
-   Patient.import patients if patient % 100 == 0 
+   Patient.import patients if patient % 1000 == 0 
 end
 
 create_diseases
 
 cases = []
-10000.times do |i|
-   c = Case.new
-   c.patient_id = rand(1..1000)
-   c.disease_id = rand(1..169)
-   c.severity = rand(0..1)
+50000.times do |i|
+   fake_casees = Case.new
+   fake_casees.patient_id = rand(1..10000)
+   fake_casees.disease_id = rand(1..169)
+   fake_casees.hospital_id = rand(1..368)
+   fake_casees.severity = rand(0..1)
 
-   cases << c
+   cases << fake_casees
 
-   Case.import cases if i % 1000 == 0 
+   Case.import cases if i % 10000 == 0 
 end
