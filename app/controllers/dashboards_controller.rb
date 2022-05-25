0f829a785f558cd3a5b063d6e1c12233ef403bf5
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-05-25 14:48:38 +0200

    Add variables to queries methods

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,13 @@
 class DashboardsController < ApplicationController
-    def index 
+    def root 
         @user = current_user 
+        @most_5_spread_cities_for_diseases = Disease.most_5_spread_cities_for_diseases
+        @count_cases =  Case.count_cases
+        @count_patients = Patient.count_patients 
+        @count_govermorates = Governorate.count_governorates
+        @count_diseases = Disease.count_diseases
+        @coun_cities = City.count_cities 
+        @most_8_diseases = Disease.most_8_diseases
+        @most_cities_have_patients = Patient.most_cities_have_patients
     end 
 end
