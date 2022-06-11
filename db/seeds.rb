b6646fa6687110d2456e8d4b46ebf8665ec9f5d9
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-06-11 15:04:52 +0200

    Create fake Doctors

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,12 +34,36 @@ end
 
 create_diseases
 
+types_of_doctors = ["Pediatricians", "Geriatric medicine", "Allergists", "Dermatologists", "Emergency physicians", 
+                    "Psychiatrists", "Obstetricians and gynecologists", "Neurologists", "Radiologists", 
+                    "Anesthesiologists", "Pediatricians", "Cardiologists", "Audiologists", "Dentists", "ENT",
+                    "Gynaecologists", "Orthopaedic surgeon", "Paediatricians", "Psychiatrists", "Pulmonologists", 
+                    "Endocrinologists", "Oncologists"]
+
+doctors = []
+
+20000.times do |doctor|
+   fake_doctors = Doctor.new
+   fake_doctors.name = Faker::Name.name
+   fake_doctors.email = Faker::Internet.email
+   fake_doctors.age = Faker::Number.between(from: 1, to: 130)
+   fake_doctors.gender = Faker::Gender.binary_type
+   fake_doctors.phone = Faker::PhoneNumber.cell_phone_in_e164
+   fake_doctors.specialization = types_of_doctors.sample
+   fake_doctors.hospital_id = rand(1..368)
+
+   doctors << fake_doctors
+   Doctor.import doctors if doctor % 8000 == 0
+end
+
+
 cases = []
 50000.times do |i|
    fake_casees = Case.new
    fake_casees.patient_id = rand(1..10000)
    fake_casees.disease_id = rand(1..169)
    fake_casees.hospital_id = rand(1..368)
+   fake_casees.doctor_id = rand(1..20000)
    fake_casees.severity = rand(0..1)
 
    cases << fake_casees
