1a9da615597a19a48f0ebe423421598e1f8ae82a
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-06-13 16:49:53 +0200

    adding governorate create action and form

--- app/controllers/governorates_controller.rb
+++ app/controllers/governorates_controller.rb
@@ -1,4 +1,22 @@
 class GovernoratesController < ApplicationController
     def index
+        @govermorates_and_its_cities = Governorate.govermorates_and_its_cities 
+        @governorates = Governorate.all 
+        @governorate = Governorate.new
     end
+
+    def create  
+        @governorate = Governorate.new(governorate_parameters)
+        if  @governorate.save 
+            redirect_to governorates_path
+        else 
+            render :index
+        end  
+    end
+
+    private
+
+    def governorate_parameters 
+        params.require(:governorate).permit(:name)
+    end  
 end
